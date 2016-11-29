70ec87deb45ebce9c40c0ae96109a987bd07af4b
Steven Simcox <steven.d.simcox@gmail.com>
2016-11-29 10:37:41 -0700

    Get it done

---
+++ app.js
@@ -0,0 +1,62 @@
+/**
+ * Created by Steven on 11/28/2016.
+ */
+var express = require('express');
+var app = require('express')();
+var nodemailer = require('nodemailer');
+var bodyParser = require('body-parser');
+var engine = require('ejs-mate');
+var ejs = require('ejs');
+var config = require('./config');
+
+app.set('view engine', 'ejs');
+app.set('/views', __dirname + "/views");
+app.use('/css', express.static('css'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.engine('ejs', engine);
+
+var smtpTransport = nodemailer.createTransport("SMTP",{
+    service:"Gmail",
+    auth:{
+        XOAuth2: {
+            user: config.mailUser,
+            clientId: config.clientId,
+            clientSecret: config.clientSecret,
+            refreshToken: config.refreshToken
+        }
+    }
+});
+
+app.get('/',function(req,res,next){
+   res.render('contact', {messageSent: false});
+});
+
+app.post('/', function(req,res,next){
+    var mailOptions = {
+        from: ''+config.mailUser+'', // sender address
+        to: req.body.email, // list of receivers
+        subject: req.body.subject, // Subject line
+        text: req.body.content, // plaintext body
+        // html: '<b>Hello world 🐴</b>' // html body
+    };
+    ejs.renderFile('./views/email.ejs',{subject:mailOptions.subject,content:mailOptions.text},function(err,str){
+        mailOptions.html = str;
+        smtpTransport.sendMail(mailOptions, function(error, info){
+            if(error){
+                console.log(error);
+                return res.send(error);
+            }
+            return res.render('contact',{messageSent: true});
+        });
+    });
+});
+
+app.get('/oauth2return',function(req,res,next){
+
+});
+
+
+
+app.listen(3000,function(){
+   console.log("Listening on port 3000");
+});
---
+++ config.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Steven on 11/29/2016.
+ */
+
+var clientId = '180547406970-ub6n94154cl4b0od1tb9dvnqpfrtqk0p.apps.googleusercontent.com';
+var clientSecret = 'a-Y31WN3tJGSPSfLRVOidjG6';
+var refreshToken = '1/1_SlW7pAwlWfwPTIVwJYB7gzsEmmzw-E4L9eaoU4VR0';
+
+
+module.exports = {
+    mailUser: 'steven.d.simcox@gmail.com',
+    mailPass: 'Espthtbl4227',
+    clientId: clientId,
+    clientSecret: clientSecret,
+    refreshToken: refreshToken
+};
