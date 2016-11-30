5a6536f155f2c0a61f1164f9a5fb73feb4aba68c
Steven Simcox <steven.d.simcox@gmail.com>
2016-11-30 16:10:24 -0700

    Edited config file

--- config.js
+++ config.js
@@ -2,14 +2,13 @@
  * Created by Steven on 11/29/2016.
  */
 
-var clientId = '180547406970-ub6n94154cl4b0od1tb9dvnqpfrtqk0p.apps.googleusercontent.com';
-var clientSecret = 'a-Y31WN3tJGSPSfLRVOidjG6';
-var refreshToken = '1/1_SlW7pAwlWfwPTIVwJYB7gzsEmmzw-E4L9eaoU4VR0';
+var clientId = '912452103028-fnlf9c0gib8mai5ff4da0eap7k1nbjlp.apps.googleusercontent.com';
+var clientSecret = 'Rk-ucMe9XUIZay_YZdB4Qp4n';
+var refreshToken = '1/ecak6k8gWCOWKUrpbibkgjcH6cvY155dLIJgjtbuw4E';
 
 
 module.exports = {
-    mailUser: '',//enter gmail email,
-    mailPass: '',//enter gmail password,
+    mailUser: 'ssimcox.dummy@gmail.com',//enter gmail email,
     clientId: clientId,
     clientSecret: clientSecret,
     refreshToken: refreshToken
